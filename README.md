0ef024590d7b3e5cf3632a4095e7c4916beb5c14
CarolMadeleine <karitomadeleine@gmail.com>
2016-09-15 11:47:37 -0500

    subiendo cambios

---
+++ README.md
@@ -0,0 +1,7 @@
+##LECCIÓN 24: BICIS - LOCAS
+
+####Contexto: Generar Validaciones
+
+###Imagen
+![Leccion 24-bicislocas](http://i65.tinypic.com/24l4t9h.png)
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+    var x = document.getElementById("name").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+     var x = document.getElementById("lastname").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+     var x = document.getElementById("input-email").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+     var x = document.getElementById("input-password").value;
+    if (x == null || x == "") {
+        alert("ERROR");
+        return false;
+    };
+
+    var email = document.getElementById("input-email").value;
+	if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email)) ) {
+  	return false;
+	}
+    
+    var x
+    x = document.getElementById("input-password").value;
+    if (isNaN(x) || x== "123456" || x== "098754" || x== "password" || x.length == 5 || /^\s+$/.test(x)) {
+      window.alert ("No Válido") ;
+    } else {
+        return true;
+    };
 }
+
